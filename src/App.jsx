e2780b748e1136c98ff13ce58963fb9bc6609513
SempaiDarcy <mr_darcy_1@mail.ru>
2023-02-27 01:09:32 +0500

    Custom Select

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css';
 import {Header} from "./components/Header";
+import {Main} from "./components/Main";
+import {Controls} from "./components/Controls";
 function App() {
   return (
     <div className="App">
      <Header/>
+        <Main>
+            <Controls/>
+        </Main>
     </div>
   );
 }
